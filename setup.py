2721b3dac5ced4f830d965bc2718cd26e1d7a199
james <jamesadinata@offerland.ca>
2025-03-13 22:39:43 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="opengeotech",
+    version="0.1.0",
+    author="OpenGeoTech Contributors",
+    author_email="your.email@example.com",
+    description="A simple geotechnical engineering package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/opengeotech",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+    ],
+    python_requires=">=3.6",
+) 
